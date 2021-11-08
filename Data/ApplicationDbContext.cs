611946870a269747c6e7188905d5646bccce7b05
AgnieszkaJania <65187859+AgnieszkaJania@users.noreply.github.com>
2021-11-08 22:16:40 +0100

    added initial migration and Customer DbSet to ApplicationDbContext with migration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LibApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,8 @@ using System.Text;
 namespace LibApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
-    {
+    {   
+        public DbSet<Customer> Customers { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
