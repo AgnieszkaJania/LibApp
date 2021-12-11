9d6cca2e6636ce0c7d3d554482ef807611ca6177
AgnieszkaJania <65187859+AgnieszkaJania@users.noreply.github.com>
2021-12-11 15:06:45 +0100

    Added server-side validation for Customer model

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -42,7 +42,8 @@ namespace LibApp.Controllers
         {
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
-            {
+            {   
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
             return View("CustomerForm",viewModel);
@@ -65,6 +66,15 @@ namespace LibApp.Controllers
         [HttpPost]
         public IActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
             if(customer.Id == 0)
             {
                 _context.Customers.Add(customer);
