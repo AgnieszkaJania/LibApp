d7d2521ca839d7aabfd827dd92887a34e9480508
AgnieszkaJania <65187859+AgnieszkaJania@users.noreply.github.com>
2022-02-22 21:14:01 +0100

    Zad 1

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -18,13 +18,16 @@ namespace LibApp.Models
             {
                 if (context.MembershipTypes.Any())
                 {
-                    Console.WriteLine("Database already seeded");
-                    return;
+                    Console.WriteLine("MembershipTypes already seeded");
+                    
                 }
-                context.MembershipTypes.AddRange(
+                else
+                {
+                    context.MembershipTypes.AddRange(
                     new MembershipType
                     {
                         Id = 1,
+                        Name = "Pay as You Go",
                         SignUpFee = 0,
                         DurationInMonths = 0,
                         DiscountRate = 0
@@ -32,6 +35,7 @@ namespace LibApp.Models
                     new MembershipType
                     {
                         Id = 2,
+                        Name = "Monthly",
                         SignUpFee = 30,
                         DurationInMonths = 1,
                         DiscountRate = 10
@@ -39,17 +43,116 @@ namespace LibApp.Models
                     new MembershipType
                     {
                         Id = 3,
+                        Name = "Quarterly",
                         SignUpFee = 90,
                         DurationInMonths = 3,
                         DiscountRate = 15
                     },
                     new MembershipType
                     {
-                         Id = 4,
-                         SignUpFee = 300,
-                         DurationInMonths = 12,
-                         DiscountRate = 20
+                        Id = 4,
+                        Name = "Yearly",
+                        SignUpFee = 300,
+                        DurationInMonths = 12,
+                        DiscountRate = 20
                     });
+                }
+                
+                // Genres
+                if (context.Genre.Any())
+                {
+                    Console.WriteLine("Genres already seeded");
+                    
+                }
+                else
+                {
+                    context.Genre.AddRange(
+                    new Genre
+                    {
+                        Id = 1,
+                        Name = "Thriller"
+                    },
+                    new Genre
+                    {
+                        Id = 2,
+                        Name = "Fantasy"
+                    },
+                    new Genre
+                    {
+                        Id = 3,
+                        Name = "Reportage"
+                    });
+                }
+                
+
+                //Customers
+                if (context.Customers.Any())
+                {
+                    Console.WriteLine("Customers already seeded");
+                    
+                }
+                else
+                {
+                    context.Customers.AddRange(
+                    new Customer
+                    {
+                        Name = "Agnieszka Jania",
+                        HasNewsletterSubscribed = true,
+                        MembershipTypeId = 1,
+                        Birthdate = new DateTime(1990, 7, 23)
+                    },
+                    new Customer
+                    {
+                        Name = "Jan Kowalski",
+                        HasNewsletterSubscribed = true,
+                        MembershipTypeId = 3,
+                        Birthdate = new DateTime(1967, 5, 12)
+                    },
+                    new Customer
+                    {
+                        Name = "Katarzyna Nowak",
+                        HasNewsletterSubscribed = false,
+                        MembershipTypeId = 1,
+                        Birthdate = new DateTime(2002, 10, 27)
+                    });
+                }
+                
+
+                //Books
+                if (context.Books.Any())
+                {
+                    Console.WriteLine("Books already seeded");
+                    
+                }
+                else
+                {
+                   context.Books.AddRange(
+                   new Book
+                   {
+                       Name = "Harry Potter i Kamien Filozoficzny",
+                       AuthorName = "J.K.Rowling",
+                       GenreId = 2,
+                       ReleaseDate = new DateTime(1990, 3, 10),
+                       NumberInStock = 20
+                   },
+                   new Book
+                   {
+                       Name = "1984",
+                       AuthorName = "George Orwell",
+                       GenreId = 3,
+                       ReleaseDate = new DateTime(1949, 6, 8),
+                       NumberInStock = 10
+                   },
+                   new Book
+                   {
+                       Name = "To",
+                       AuthorName = "Stephen King",
+                       GenreId = 1,
+                       ReleaseDate = new DateTime(1986, 9, 15),
+                       NumberInStock = 50
+                   });
+                }
+               
                 context.SaveChanges();
             }
         }
